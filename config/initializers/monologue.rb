33a99db76551a75fc48703fcf3b9b6ad15b357a9
Ryan Kelley <rskelley9@gmail.com>
2014-05-28 06:32:21 -0400

    WIP styling monologue layout

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -1,5 +1,9 @@
 Monologue::PageCache.enabled = true
 
+Monologue.layout               = "layouts/blog/blog-custom-alternate" # set the layout you want to use if you want to use your main_app layout
+
+# Monologue.google_analytics_id  = "GA-CODE-HERE" # OPTIONAL: Your Google Analytics code should be here if you have one.
+
 # Monologue.sidebar              = ["latest_posts", "latest_tweets"]   # this will add the latests posts and latests tweets in the right sidebar.
 # Monologue.show_rss_icon        = true # will show the RSS icon (with link) in the header)
 # Monologue.facebook_url         = "https://www.facebook.com/jipiboilycom" # if set, this will enable Facebook icon and link it to your Facebook page.
@@ -8,3 +12,16 @@ Monologue::PageCache.enabled = true
 # Monologue.github_username      = "http://github.com/jipiboily"  # if set, will enable Github icon and link to this URL.
 # Monologue.gauge_analytics_site_id = "your-gaug.es-id-here" # add your [Gaug.es](http://get.gaug.es/) id here to enable it.
 # Monologue.facebook_logo           = 'logo.png'  # used in the open graph protocol to display an image when a post is liked
+
+# Monologue.site_name            = "FreeSwim"
+# Monologue.site_subtitle        = "Thoughts, reviews, opinions from the FreeSwim team."
+# Monologue.site_url             = "www.freeswim.co/blog-posts"
+# Monologue.disqus_shortname     = "example_disqus_username" # register on disqus.com if you don't have one
+# Monologue.meta_description     = "This is the description that will be seen on Google by default for the main page"
+# Monologue.meta_keyword         = "those, are, meta, key, words, that, you, should, enter, comma, delimited, just, like, this"
+Monologue.twitter_username     = "twitter_user_name"
+# Monologue.twitter_locale       = "en" # This is the locale for twitter. "en" for english or "fr" for french. There is much more!
+# Monologue.facebook_like_locale = "en_US" # same, but for facebook. French is "fr_CA"
+# Monologue.google_plusone_locale = "en" # same for Google +
+# Monologue.admin_force_ssl       = true # that can be set to false if you don't have SSL, but it is not recommended
+Monologue.posts_per_page      = 5 # OPTIONAL: number of posts per page on main page. It defaults to 10 currently
